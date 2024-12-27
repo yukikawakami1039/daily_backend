04cd12bf1400cea350346427b133b9f8bfa3da84
mayo <3cdi1208@tokai.ac.jp>
2024-12-27 12:46:45 +0900

    dec27

--- server.js
+++ server.js
@@ -1,20 +1,17 @@
 // backend/server.js
 const express = require('express');
-const mongoose = require('mongoose');
-const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
 const PORT = process.env.PORT || 3000;
 const auth = require('./src/routes/auth');
 const connectDB = require('./src/config/db');
 
+const app = express();
 // JSONボディパーサ
 app.use(express.json());
 //authのルーティング
 app.use('/auth', auth);
 
-//
-app.get('/', (req, res) => {
-  res.send('Backend server is running');
-});
 //データベースに接続
 connectDB();
 
