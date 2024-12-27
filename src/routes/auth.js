780ac908d1a7b8cfe40af1d4965722ef9c910eb9
yukikawakami1039 <3cdi1208@toka.ac.jp>
2024-12-27 13:33:57 +0900

    notepc-branch から src/routes/auth.js を取り込み

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -8,7 +8,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 const mongoose = require('mongoose');
 //ログイン
-router.get('/', (req, res) => {
+router.get('/auth', (req, res) => {
   res.send('auth');
 });
 
@@ -18,37 +18,41 @@ router.post('/register',
     body('email').isEmail(), 
     body('password').isLength({ min: 6 }), 
     async (req, res) => {
-        const userName = req.body.userName;
-        const email = req.body.email;
-        const password = req.body.password;
-        //バリデーション
-        const errors = validationResult(req);
-     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+        try {
+            const userName = req.body.userName;
+            const email = req.body.email;
+            const password = req.body.password;
+            //バリデーション
+            const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        //ユーザーが存在するか確認
+        const user = await User.findOne({ email: email });
+        if (user) {
+            return res.status(400).json([
+                {
+                    message: 'すでにそのユーザーが存在しています'
+                }
+            ]);
+        }
+        //パスワードをハッシュ化
+        let hashedPassword = await bcrypt.hash(password, 10);
+
+        //dbに保存
+        const newUser = await User.create({ userName, email, password: hashedPassword });
+        await newUser.save();
+
+        //JWTを発行
+        const token = jwt.sign({ id: newUser._id, email: newUser.email },
+        process.env.JWT_SECRET,
+        { expiresIn: '24h' }
+        );
+
+        return res.status(200).json({ token });
+    } catch (error) {
+        return res.status(500).json({ message: 'サーバーエラー' });
     }
-    //ユーザーが存在するか確認
-    const user = User.find((user) => user.email === email);
-    if (user) {
-        return res.status(400).json([
-             {
-                message: 'すでにそのユーザーが存在しています'
-            }
-        ]);
-    }
-    //パスワードをハッシュ化
-    let hashedPassword = await bcrypt.hash(password, 10);
-
-    //dbに保存
-    User.push({ userName, email, password: hashedPassword });
-    //res.status(201).json({ message: 'ユーザー登録が完了しました' });
-
-    //JWTを発行
-    const token = jwt.sign({ email, },
-    process.env.JWT_SECRET,
-    { expiresIn: '24h' }
-    );
-
-    return res.status(200).json({ token });
 });
 
 //ログイン用API
@@ -61,7 +65,7 @@ router.post('/login', async (req, res) => {
         return res.status(400).json([{ message: 'ユーザーが存在しません' }]);
     }
 
-    //パスワードを比較
+    //パスワードの複合、照合
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
         return res.status(400).json([{ message: 'パスワードが間違っています' }]);
@@ -75,8 +79,13 @@ router.post('/login', async (req, res) => {
 
 
 //DBのユーザーを確認
-router.get("/allUsers", (req, res) => {
-    return res.json(User);
+router.get("/allUsers", async (req, res) => {
+    const users = await User.find({},{password:0});
+    try {
+        return res.status(200).json(users);
+    } catch (error) {
+        return res.status(500).json({ message: 'サーバーエラー' });
+    }
 });
 
 //得たユーザー情報をもとに日記の記入等進める
