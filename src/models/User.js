33b5e20feabc3838d64ef0383422e25160b79eac
yukikawakami1039 <3cdi1208@toka.ac.jp>
2024-12-27 13:41:43 +0900

    notepc-branch から src/models/Use.js を取り込み

--- src/models/User.js
+++ src/models/User.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 //ユーザーのデータを管理する
 const userSchema = new mongoose.Schema({
-    userName: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    userName: { type: String, required: true, unique: true, minlength: 3, maxlength: 20 },
+    email: { type: String, required: true, unique: true, minlength: 3 },
+    password: { type: String, required: true, minlength: 6 }
 });
 
 const User = mongoose.model('User', userSchema);
